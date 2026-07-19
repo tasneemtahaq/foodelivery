3c2646fb4e86ed383fe15d347cd426994a1b08f9
tasneemtahaq <tasneemtahaq@gmail.com>
2026-07-19 22:37:11 +0500

    Database updated

--- src/app/admin/categories/AdminCategoriesClient.tsx
+++ src/app/admin/categories/AdminCategoriesClient.tsx
@@ -106,7 +106,7 @@ export default function AdminCategoriesClient({
       </div>
 
       {/* Categories Grid */}
-      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4">
+      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-2  gap-4">
         {categories.map((cat, i) => (
           <motion.div
             key={cat.id}
